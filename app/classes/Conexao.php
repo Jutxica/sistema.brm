c2cd449008406bddd134cdf973d60e5765992bd4
João Rodrigues Utxica <jutxica2202@gmail.com>
2026-08-26 23:20:58 -0300

    feat: add dynamic mysql configurations via env vars in Conexao and render.yaml

--- app/classes/Conexao.php
+++ app/classes/Conexao.php
@@ -9,14 +9,20 @@ class Conexao {
     public function conectar(){
         try{
             if(!isset(self::$conexao)):
+                $host = getenv('DB_HOST') ?: '127.0.0.1';
+                $dbname = getenv('DB_NAME') ?: 'conven80_sistema';
+                $user = getenv('DB_USER') ?: 'conven80_sistema';
+                $pass = getenv('DB_PASSWORD') ?: 'yZ0QSbfF)m!]';
+                $port = getenv('DB_PORT') ?: '3306';
+
                 self::$conexao = new \PDO(
-                    "mysql:host=127.0.0.1;dbname=conven80_sistema;charset=utf8",
-                    "conven80_sistema",
-                    "yZ0QSbfF)m!]"
+                    "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
+                    $user,
+                    $pass
                 );
             endif;
         } catch (\PDOException $e){
-            echo "Erro ao conectar ao banco" . $e->getMessage();
+            echo "Erro ao conectar ao banco: " . $e->getMessage();
         }
         return self::$conexao;
     }
